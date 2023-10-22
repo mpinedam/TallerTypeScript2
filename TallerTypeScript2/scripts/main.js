5d57d8d7c60fbd98f707ec9d74d26d851e8ffb06
Mariana Pineda Miranda <111026594+mpinedam@users.noreply.github.com>
2023-10-22 22:27:34 +0000

    commit final

--- TallerTypeScript2/scripts/main.js
+++ TallerTypeScript2/scripts/main.js
@@ -9,7 +9,6 @@ function mostrarDatosSerie(allseries) {
     });
 }
 function mostrarCard(serie) {
-    console.log(serie);
     var card = document.getElementById('card');
     card.innerHTML = "\n      <div class=\"card\" style=\"width: 18rem;\">\n      <img class=\"card-img-top\" src=".concat(serie.image, " alt=").concat(serie.name, ">\n      <div class=\"card-body\">\n        <h5 class=\"card-title\">").concat(serie.name, "</h5>\n        <p class=\"card-text\">").concat(serie.description, "</p>\n        <a href=").concat(serie.link, " class=\"btn btn-primary\">Link ver serie</a>\n      </div>\n    </div>\n  ");
 }
