302311efed8b5b322152dfa4925a71de735a8612
Mariana Pineda Miranda <111026594+mpinedam@users.noreply.github.com>
2023-10-22 21:56:18 +0000

    commit card seen

--- TallerTypeScript2/main.ts
+++ TallerTypeScript2/main.ts
@@ -1,5 +1,4 @@
 import { series } from "./series.js";
-
 import { listaseries } from "./data.js";
 
 let seriesTable: HTMLElement = document.getElementById("serie")!;
@@ -7,11 +6,30 @@ let seriesTable: HTMLElement = document.getElementById("serie")!;
 mostrarDatosSerie(listaseries);
 
 function mostrarDatosSerie(allseries: series[]): void {
-  
   allseries.forEach((serie) => {
     let tbodyAprendiz = document.createElement("tr");
-    tbodyAprendiz.innerHTML = `<tr><td>${serie.id}</td> <td><a href="${serie.name}" target=${serie.link}>${serie.name}</a></td> <td>${serie.channel}</td> <td>${serie.seasons}</td> </tr>`;
+    tbodyAprendiz.innerHTML = `
+      <td>${serie.id}</td>
+      <td>
+        <a href="#" onclick=${mostrarCard(serie)}>${serie.name}</a>
+      </td>
+      <td>${serie.channel}</td>
+      <td>${serie.seasons}</td>
+    `;
     seriesTable.appendChild(tbodyAprendiz);
-  }) ;
+  });
+}
 
+function mostrarCard(serie: series): void {
+  let card:HTMLElement = document.getElementById('card')!;
+  card.innerHTML = `
+      <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src=${serie.image} alt=${serie.name}>
+      <div class="card-body">
+        <h5 class="card-title">${serie.name}</h5>
+        <p class="card-text">${serie.description}</p>
+        <a href=${serie.link} class="btn btn-primary">Link ver serie</a>
+      </div>
+    </div>
+  `;
 }
--- TallerTypeScript2/scripts/main.js
+++ TallerTypeScript2/scripts/main.js
@@ -4,7 +4,12 @@ mostrarDatosSerie(listaseries);
 function mostrarDatosSerie(allseries) {
     allseries.forEach(function (serie) {
         var tbodyAprendiz = document.createElement("tr");
-        tbodyAprendiz.innerHTML = "<tr><td>".concat(serie.id, "</td> <td><a href=\"").concat(serie.name, "\" target=").concat(serie.link, ">").concat(serie.name, "</a></td> <td>").concat(serie.channel, "</td> <td>").concat(serie.seasons, "</td> </tr>");
+        tbodyAprendiz.innerHTML = "\n      <td>".concat(serie.id, "</td>\n      <td>\n        <a href=\"#\" onclick=").concat(mostrarCard(serie), ">").concat(serie.name, "</a>\n      </td>\n      <td>").concat(serie.channel, "</td>\n      <td>").concat(serie.seasons, "</td>\n    ");
         seriesTable.appendChild(tbodyAprendiz);
     });
+}
+function mostrarCard(serie) {
+    console.log(serie);
+    var card = document.getElementById('card');
+    card.innerHTML = "\n      <div class=\"card\" style=\"width: 18rem;\">\n      <img class=\"card-img-top\" src=".concat(serie.image, " alt=").concat(serie.name, ">\n      <div class=\"card-body\">\n        <h5 class=\"card-title\">").concat(serie.name, "</h5>\n        <p class=\"card-text\">").concat(serie.description, "</p>\n        <a href=").concat(serie.link, " class=\"btn btn-primary\">Link ver serie</a>\n      </div>\n    </div>\n  ");
 }
